eb6d978d08f9f0c0885b3a4e24487070f03db18c
Catherine <katerinasheps@gmail.com>
2021-12-19 13:27:33 +0300

    Added 9_task

---
+++ 9_task/4_hint.cpp
@@ -0,0 +1,28 @@
+#include <map>
+#include <iostream>
+
+
+int main (int argc, char* argv[]) {
+    std::map<int, int> myMap;
+    size_t size = 0;
+    for (size_t i = 0; i < 1000; i++) {
+        if (i == 200) {
+            continue;
+        } 
+        myMap.insert({i, i});
+        if (std::size(myMap) == size) {
+            std::cout << "Bad insert" << std::endl;
+        }
+        size++;
+    }
+    
+    for (size_t i = 0; i < 500000; i++) {
+        auto it = myMap.find(201);
+        myMap.insert(it, {200, 200});
+        if (std::size(myMap) == size) {
+            std::cout << "Bad insert new" << std::endl;
+        }
+        myMap.erase(200);
+    }
+    return 0;
+}
